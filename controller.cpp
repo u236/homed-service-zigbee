8de3f8de77fe5f864d319128522a2ec24159ef21
u236 <mail@u236.org>
2022-07-30 10:51:57 +0300

    configureDevice commmand added

--- controller.cpp
+++ controller.cpp
@@ -39,12 +39,13 @@ void Controller::mqttReceived(const QByteArray &message, const QMqttTopicName &t
     }
     else if (topic.name() == "homed/command/zigbee" && json.contains("action"))
     {
-        QList <QString> list = {"setPermitJoin", "removeDevice"};
+        QList <QString> list = {"setPermitJoin", "configureDevice", "removeDevice"};
 
         switch (list.indexOf(json.value("action").toString()))
         {
             case 0:  m_zigbee->setPermitJoin(json.value("enabled").toBool()); break;
-            case 1: m_zigbee->removeDevice(QByteArray::fromHex(json.value("deviceAddress").toString().toUtf8())); break;
+            case 1: m_zigbee->configureDevice(QByteArray::fromHex(json.value("deviceAddress").toString().toUtf8())); break;
+            case 2: m_zigbee->removeDevice(QByteArray::fromHex(json.value("deviceAddress").toString().toUtf8())); break;
         }
     }
     else if (topic.name().startsWith("homed/td/zigbee/"))
