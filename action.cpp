55b88106f088b3a1308669cb7ac1d50c3f1fa8e9
u236 <mail@u236.org>
2023-04-20 10:23:17 +0300

    actions clean up

--- action.cpp
+++ action.cpp
@@ -125,19 +125,18 @@ QByteArray Actions::Level::request(const QString &, const QVariant &data)
         case QVariant::String:
         {
             QList <QString> list {"moveLevelUp", "moveLevelDown", "stopLevel"}; // TODO: add step actions
-            QString action = data.toString();
+            int index = list.indexOf(data.toString());
 
-            switch (list.indexOf(data.toString()))
+            switch (index)
             {
-                case 0:
-                case 1:
+                case 0 ... 1:
                 {
                     moveLevelStruct payload;
 
-                    payload.mode = action == "moveLevelUp" ? 0x00 : 0x01;
+                    payload.mode = index ? 0x01 : 0x00;
                     payload.rate = 0x55;
 
-                    return zclHeader(FC_CLUSTER_SPECIFIC, m_transactionId++, "moveLevelUp" ? 0x05 : 0x01).append(reinterpret_cast <char*> (&payload), sizeof(payload));
+                    return zclHeader(FC_CLUSTER_SPECIFIC, m_transactionId++, index ? 0x01 : 0x05).append(reinterpret_cast <char*> (&payload), sizeof(payload));
                 }
 
                 case 2:  return zclHeader(FC_CLUSTER_SPECIFIC, m_transactionId++, 0x07);
@@ -803,7 +802,9 @@ QByteArray ActionsTUYA::WaterValve::request(const QString &name, const QVariant 
 
 QByteArray ActionsTUYA::PresenceSensor::request(const QString &name, const QVariant &data)
 {
-    switch (m_actions.indexOf(name))
+    int index = m_actions.indexOf(name);
+
+    switch (index)
     {
         case 0: // sensitivity
         {
@@ -817,16 +818,6 @@ QByteArray ActionsTUYA::PresenceSensor::request(const QString &name, const QVari
         }
 
         case 1: // distanceMin
-        {
-            quint32 value = static_cast <quint32> (data.toDouble() * 100);
-
-            if (value > 950)
-                value = 950;
-
-            value = qToBigEndian(value);
-            return makeRequest(m_transactionId++, 0x03, TUYA_TYPE_VALUE, &value);
-        }
-
         case 2: // distanceMax
         {
             quint32 value = static_cast <quint32> (data.toDouble() * 100);
@@ -835,7 +826,7 @@ QByteArray ActionsTUYA::PresenceSensor::request(const QString &name, const QVari
                 value = 950;
 
             value = qToBigEndian(value);
-            return makeRequest(m_transactionId++, 0x04, TUYA_TYPE_VALUE, &value);
+            return makeRequest(m_transactionId++, index == 1 ? 0x03 : 0x04, TUYA_TYPE_VALUE, &value);
         }
 
         case 3: // detectionDelay
@@ -1055,24 +1046,21 @@ QByteArray ActionsEfekta::TemperatureSettings::request(const QString &name, cons
         case 0: // temperatureOffset
         {
             qint16 value = qToLittleEndian <qint16> (data.toDouble() * 10);
-            m_attributes = {0x0210};
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_16BIT_SIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, 0x0210, DATA_TYPE_16BIT_SIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
 
         case 1: // temperatureHigh
         case 2: // temperatureLow
         {
             qint16 value = qToLittleEndian <qint16> (data.toInt());
-            m_attributes = {static_cast <quint16> (index == 1 ? 0x0221 : 0x2222)};
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_16BIT_SIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, index == 1 ? 0x0221 : 0x2222, DATA_TYPE_16BIT_SIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
 
         case 3: // temperatureRelay
         case 4: // temperatureRelayInvert
         {
             quint8 value = data.toBool() ? 0x01 : 0x00;
-            m_attributes = {static_cast <quint16> (index == 3 ? 0x0220 : 0x0225)};
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_BOOLEAN, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, index == 3 ? 0x0220 : 0x0225, DATA_TYPE_BOOLEAN, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
     }
 
@@ -1088,24 +1076,21 @@ QByteArray ActionsEfekta::HumiditySettings::request(const QString &name, const Q
         case 0: // humidityOffset
         {
             qint16 value = qToLittleEndian <qint16> (data.toInt());
-            m_attributes = {0x0210};
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_16BIT_SIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, 0x0210, DATA_TYPE_16BIT_SIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
 
         case 1: // humidityHigh
         case 2: // humidityLow
         {
             quint8 value = qToLittleEndian <quint8> (data.toInt());
-            m_attributes = {static_cast <quint16> (index == 1 ? 0x0221 : 0x2222)};
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_16BIT_SIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, index == 1 ? 0x0221 : 0x2222, DATA_TYPE_16BIT_SIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
 
         case 3: // humidityRelay
         case 4: // humidityRelayInvert
         {
             quint8 value = data.toBool() ? 0x01 : 0x00;
-            m_attributes = {static_cast <quint16> (index == 3 ? 0x0220 : 0x0225)};
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_BOOLEAN, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, index == 3 ? 0x0220 : 0x0225, DATA_TYPE_BOOLEAN, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
     }
 
@@ -1118,50 +1103,47 @@ QByteArray ActionsEfekta::CO2Sensor::request(const QString &name, const QVariant
 
     switch (index)
     {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
+        case 0 ... 3:
         {
-            quint16 value = qToLittleEndian <quint16> (data.toInt());
+            quint16 attributeId, value = qToLittleEndian <quint16> (data.toInt());
 
             switch (index)
             {
-                case 0:  m_attributes = {0x0205}; break; // altitude
-                case 1:  m_attributes = {0x0207}; break; // manualCalibration
-                case 2:  m_attributes = {0x0221}; break; // co2High
-                case 3:  m_attributes = {0x0222}; break; // co2Low
+                case 0:  attributeId = 0x0205; break; // altitude
+                case 1:  attributeId = 0x0207; break; // manualCalibration
+                case 2:  attributeId = 0x0221; break; // co2High
+                case 3:  attributeId = 0x0222; break; // co2Low
             }
 
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_16BIT_UNSIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, attributeId, DATA_TYPE_16BIT_UNSIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
 
         case 4: // indicatorLevel
         {
             quint8 value = qToLittleEndian <quint8> (data.toInt());
-            m_attributes = {0x0209};
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_8BIT_UNSIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, 0x0209, DATA_TYPE_8BIT_UNSIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
 
         default:
         {
+            quint16 attributeId;
             quint8 value = data.toBool() ? 0x01 : 0x00;
 
             switch (index)
             {
-                case 5:  m_attributes = {0x0202}; break; // forceCalibration
-                case 6:  m_attributes = {0x0203}; break; // autoBrightness
-                case 7:  m_attributes = {0x0204}; break; // co2LongChart
-                case 8:  m_attributes = {0x0206}; break; // co2FactoryReset
-                case 9:  m_attributes = {0x0211}; break; // indicator
-                case 10: m_attributes = {0x0220}; break; // co2Relay
-                case 11: m_attributes = {0x0225}; break; // co2RelayInvert
-                case 12: m_attributes = {0x0244}; break; // pressureLongChart
-                case 13: m_attributes = {0x0401}; break; // nightBacklight
+                case 5:  attributeId = 0x0202; break; // forceCalibration
+                case 6:  attributeId = 0x0203; break; // autoBrightness
+                case 7:  attributeId = 0x0204; break; // co2LongChart
+                case 8:  attributeId = 0x0206; break; // co2FactoryReset
+                case 9:  attributeId = 0x0211; break; // indicator
+                case 10: attributeId = 0x0220; break; // co2Relay
+                case 11: attributeId = 0x0225; break; // co2RelayInvert
+                case 12: attributeId = 0x0244; break; // pressureLongChart
+                case 13: attributeId = 0x0401; break; // nightBacklight
                 default: return QByteArray();
             }
 
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_BOOLEAN, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, attributeId, DATA_TYPE_BOOLEAN, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
     }
 }
@@ -1176,16 +1158,14 @@ QByteArray ActionsEfekta::VOCSensor::request(const QString &name, const QVariant
         case 2: // vocLow
         {
             quint16 value = qToLittleEndian <quint16> (data.toInt());
-            m_attributes = {static_cast <quint16> (index == 1 ? 0x0221 : 0x2222)};
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_16BIT_UNSIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, index == 1 ? 0x0221 : 0x2222, DATA_TYPE_16BIT_UNSIGNED, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
 
         case 3: // vocRelay
         case 4: // vocRelayInvert
         {
             quint8 value = data.toBool() ? 0x01 : 0x00;
-            m_attributes = {static_cast <quint16> (index == 3 ? 0x0220 : 0x0225)};
-            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, m_attributes.at(0), DATA_TYPE_BOOLEAN, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
+            return writeAttributeRequest(m_transactionId++, m_manufacturerCode, index == 3 ? 0x0220 : 0x0225, DATA_TYPE_BOOLEAN, QByteArray(reinterpret_cast <char*> (&value), sizeof(value)));
         }
     }
 
