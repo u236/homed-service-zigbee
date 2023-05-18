e8b3f0512d1054d07ada613b90f0522c4ff789ed
u236 <mail@u236.org>
2023-05-18 10:25:19 +0300

    shared linking and pipeline cleanup

--- controller.h
+++ controller.h
@@ -1,7 +1,7 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#define SERVICE_VERSION                 "3.2.5"
+#define SERVICE_VERSION                 "3.3.0"
 #define UPDATE_AVAILABILITY_INTERVAL    10000
 
 #include "homed.h"
