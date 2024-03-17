8450db5ce69e4c2ed59e119e3f857dd26baa6f44
u236 <mail@u236.org>
2024-03-17 19:51:20 +0300

    clean up

--- device.cpp
+++ device.cpp
@@ -343,7 +343,7 @@ void DeviceList::setupEndpoint(const Endpoint &endpoint, const QJsonObject &json
             for (auto it = options.begin(); it != options.end(); it++)
             {
                 QMap <QString, QVariant> option = it.value().toMap();
-                Property property(new PropertiesCustom::Attribute(it.key(), option.value("type").toString(), static_cast <quint16> (option.value("clusterId").toInt()), static_cast <quint16> (option.value("attributeId").toInt()), static_cast <quint8> (option.value("dataType").toInt()), option.value("divider", 1).toDouble() * option.value("propertyDivider", 1).toDouble()));
+                Property property(new PropertiesCustom::Attribute(it.key(), option.value("type").toString(), static_cast <quint16> (option.value("clusterId").toInt()), static_cast <quint16> (option.value("attributeId").toInt()), static_cast <quint8> (option.value("dataType").toInt()), option.value("divider", 1).toDouble()));
                 property->setParent(endpoint.data());
                 property->setMultiple(multiple);
                 endpoint->properties().append(property);
@@ -379,7 +379,7 @@ void DeviceList::setupEndpoint(const Endpoint &endpoint, const QJsonObject &json
                 if (!option.value("action").toBool())
                     continue;
 
-                action = Action(new ActionsCustom::Attribute(it.key(), option.value("type").toString(), static_cast <quint16> (option.value("clusterId").toInt()), static_cast <quint16> (option.value("manufacturerCode").toInt()), static_cast <quint16> (option.value("attributeId").toInt()), static_cast <quint8> (option.value("dataType").toInt()), option.value("divider", 1).toDouble() * option.value("actionDivider", 1).toDouble()));
+                action = Action(new ActionsCustom::Attribute(it.key(), option.value("type").toString(), static_cast <quint16> (option.value("clusterId").toInt()), static_cast <quint16> (option.value("manufacturerCode").toInt()), static_cast <quint16> (option.value("attributeId").toInt()), static_cast <quint8> (option.value("dataType").toInt()), option.value("divider", 1).toDouble()));
                 action->setParent(endpoint.data());
                 endpoint->actions().append(action);
             }
