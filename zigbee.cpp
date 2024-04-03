7fb2c5d6c4eff2659cbba0ba8c6c316b60dc3b03
u236 <mail@u236.org>
2024-04-03 22:45:22 +0300

    miboxer remote added

--- zigbee.cpp
+++ zigbee.cpp
@@ -561,6 +561,19 @@ bool ZigBee::interviewQuirks(const Device &device)
     if (device->options().value("tuyaDataQuery").toBool())
         enqueueRequest(device, 0x01, CLUSTER_TUYA_DATA, zclHeader(FC_CLUSTER_SPECIFIC, m_requestId, 0x03), "data query request");
 
+    if (device->manufacturerName() == "_TZ3000_xwh1e22x")
+    {
+        QByteArray payload = QByteArray(1, 0x08);
+
+        for (quint8 i = 0; i < 8; i++)
+        {
+            quint16 value = qToLittleEndian <quint16> (i + 1001);
+            payload.append(reinterpret_cast <char*> (&value), sizeof(value)).append(1, i + 1);
+        }
+
+        enqueueRequest(device, 0x01, CLUSTER_GROUPS, zclHeader(FC_CLUSTER_SPECIFIC | FC_DISABLE_DEFAULT_RESPONSE, m_requestId, 0xF0).append(payload), "groups setup request");
+    }
+
     return true;
 }
 
