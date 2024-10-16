934a34be34dfd7433299659e3f565fc6014859ec
u236 <mail@u236.org>
2024-10-16 17:10:26 +0300

    aqara button action updated

--- properties/lumi.cpp
+++ properties/lumi.cpp
@@ -407,8 +407,7 @@ void PropertiesLUMI::ButtonAction::parseAttribte(quint16, quint16 attributeId, c
 
     switch (static_cast <quint8> (data.at(0)))
     {
-        case 0x00: m_value = "on"; break;
-        case 0x01: m_value = "off"; break;
+        case 0x00: m_value = "singleClick"; break;
         case 0x02: m_value = "doubleClick"; break;
         case 0x03: m_value = "tripleClick"; break;
         case 0x04: m_value = "quadrupleClick"; break;
