626b54ce8771e24f7e83991cec303bdff765409a
u236 <mail@u236.org>
2025-10-07 10:22:41 +0300

    little fix

--- controller.h
+++ controller.h
@@ -1,7 +1,7 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#define SERVICE_VERSION                 "3.10.3"
+#define SERVICE_VERSION                 "3.10.4"
 #define UPDATE_DEVICE_DATA_INTERVAL     5000
 #define UPDATE_PROPERTIES_DELAY         1000
 
