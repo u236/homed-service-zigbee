c9b2f735e23ff771b5a4c07a753180193e8ef2ce
u236 <mail@u236.org>
2026-01-15 00:06:01 +0300

    add new ts0001 relay

--- controller.h
+++ controller.h
@@ -1,7 +1,7 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#define SERVICE_VERSION                 "3.11.3"
+#define SERVICE_VERSION                 "3.12.0"
 #define UPDATE_DEVICE_DATA_INTERVAL     5000
 #define UPDATE_PROPERTIES_DELAY         1000
 
